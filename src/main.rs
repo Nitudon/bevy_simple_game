a7e20e3e5846630667a83d49186eb9a577f1526c
udon <myudongen@gmail.com>
2021-08-08 01:44:40 +0900

    draw player and stage sprites

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,16 @@ use game::system::setup::setup;
 
 fn main() {
     App::build()
+        .insert_resource(WindowDescriptor {
+            title: "Apple Game".to_string(),
+            width: 960.,
+            height: 540.,
+            vsync: true,
+            ..Default::default()
+        })
         .add_plugins(DefaultPlugins)
         .add_plugin(GamePlugin)
-        .insert_resource(ClearColor(Color::rgb(0.9, 0.9, 0.9)))
         .add_startup_system(setup.system())
+        .insert_resource(ClearColor(Color::rgb(0.9, 0.9, 0.9)))
         .run();
 }
--- src/system/setup.rs
+++ src/system/setup.rs
@@ -1,7 +1,20 @@
 use bevy::ecs::system::Commands;
 use bevy::prelude::*;
 
-pub fn setup(mut commands: Commands) {
+pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut materials: ResMut<Assets<ColorMaterial>>) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
     commands.spawn_bundle(UiCameraBundle::default());
+
+    let stage_texture_handle = asset_server.load("textures/stage.png");
+    commands.spawn_bundle(SpriteBundle {
+        material: materials.add(stage_texture_handle.into()),
+        sprite: Sprite::new(Vec2::new(960., 540.)),
+        ..Default::default()
+    });
+    let player_texture_handle = asset_server.load("textures/player.png");
+    commands.spawn_bundle(SpriteBundle {
+        material: materials.add(player_texture_handle.into()),
+        sprite: Sprite::new(Vec2::new(90., 90.)),
+        ..Default::default()
+    });
 }
