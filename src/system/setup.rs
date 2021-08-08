c5442c79801e31b02f4823cc02d8b5e93e0895e6
udon <myudongen@gmail.com>
2021-08-08 20:16:17 +0900

    insert player bundle with components, enable to move by keyborad

--- src/system/setup.rs
+++ src/system/setup.rs
@@ -1,5 +1,7 @@
 use bevy::ecs::system::Commands;
 use bevy::prelude::*;
+use component::player::Player;
+use component::mover::Mover;
 
 pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut materials: ResMut<Assets<ColorMaterial>>) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
@@ -11,10 +13,23 @@ pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>, mut materia
         sprite: Sprite::new(Vec2::new(960., 540.)),
         ..Default::default()
     });
+    
     let player_texture_handle = asset_server.load("textures/player.png");
-    commands.spawn_bundle(SpriteBundle {
+    let position = Vec3::new(0., -200., 0.);
+    let rotation = Quat::from_axis_angle(Vec3::Y, 0.);
+    let scale = Vec3::splat(1.);
+    commands
+        .spawn_bundle(SpriteBundle {
         material: materials.add(player_texture_handle.into()),
         sprite: Sprite::new(Vec2::new(90., 90.)),
-        ..Default::default()
-    });
+        transform: Transform {
+            translation: position,
+            rotation: rotation,
+            scale: scale,
+        },
+        ..Default::default() })
+        .insert_bundle((
+            Player::default(),
+            Mover::default()
+            ));
 }
